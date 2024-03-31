94e9dbbbd93cb01e63c3e70675b9ce37b794b57c
Taro Tanaka <mkrmdk@gmail.com>
2024-04-01 02:56:54 +0900

    Use user-data instead of the deprecated shared-script-properties

--- youtube-ui.lua
+++ youtube-ui.lua
@@ -1907,15 +1907,12 @@ function update_margins()
         reset_margins()
     end
 
-    utils.shared_script_property_set("osc-margins",
-        string.format("%f,%f,%f,%f", margins.l, margins.r, margins.t, margins.b))
-    -- mp.set_property_native("user-data/osc/margins", margins)
+    mp.set_property_native("user-data/osc/margins", margins)
 end
 
 function shutdown()
     reset_margins()
-    utils.shared_script_property_set("osc-margins", nil)
-    -- mp.del_property("user-data/osc")
+    mp.del_property("user-data/osc")
 end
 
 --
@@ -2567,8 +2564,7 @@ function visibility_mode(mode, no_osd)
     end
 
     user_opts.visibility = mode
-    utils.shared_script_property_set("osc-visibility", mode)
-    -- mp.set_property_native("user-data/osc/visibility", mode)
+    mp.set_property_native("user-data/osc/visibility", mode)
 
     if not no_osd and tonumber(mp.get_property("osd-level")) >= 1 then
         mp.osd_message("OSC visibility: " .. mode)
@@ -2600,8 +2596,7 @@ function idlescreen_visibility(mode, no_osd)
         user_opts.idlescreen = false
     end
 
-    utils.shared_script_property_set("osc-idlescreen", mode)
-    -- mp.set_property_native("user-data/osc/idlescreen", user_opts.idlescreen)
+    mp.set_property_native("user-data/osc/idlescreen", user_opts.idlescreen)
 
     if not no_osd and tonumber(mp.get_property("osd-level")) >= 1 then
         mp.osd_message("OSC logo visibility: " .. tostring(mode))
